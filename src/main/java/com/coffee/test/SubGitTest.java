f1dfbcd67fe47adc038baedf81954bf6d12b5d81
songhwaseong <supyoungsun@gmail.com>
2026-05-06 22:22:38 +0900

    sub git test

---
+++ src/main/java/com/coffee/test/SubGitTest.java
@@ -0,0 +1,9 @@
+package com.coffee.test;
+
+public class SubGitTest {
+    public static void main(String[] args) {
+        System.out.println("subtest");
+        System.out.println("subtest");
+        System.out.println("subtest");
+    }
+}
